80b48a3655e920ee1be6f808f40925b657dea06d
rick <edcrfvlily5@gmail.com>
2021-12-22 18:15:44 +0800

    [modify] 樣式修改

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -6,66 +6,40 @@ export default function Index() {
       className={"flex flex-wrap"}
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
     >
-      <div
+      <Link
+        to={"/robots"}
         className={
-          "m-5 shadow-2xl p-2 shadow-indigo-300 rounded-2xl text-center"
+          "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
         }
       >
-        <h1>預載 星際大戰人物資料</h1>
-        <button
-          className={
-            "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
-          }
-        >
-          <Link prefetch="intent" to={"/posts"}>
-            Posts
-          </Link>
-        </button>
-      </div>
-      <div
+        星際大戰與機器人
+      </Link>
+      <Link
+        prefetch="intent"
+        to={"/posts"}
         className={
-          "m-5 shadow-2xl p-2 shadow-indigo-400 rounded-2xl text-center"
+          "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
         }
       >
-        <h1>星際大戰人物資料</h1>
-        <button
-          className={
-            "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
-          }
-        >
-          <Link to={"/posts"}>Posts Not intent</Link>
-        </button>
-      </div>
-      <div
+        預載 星際大戰人物資料
+      </Link>
+      <Link
+        to={"/robots"}
         className={
-          "m-5 shadow-2xl p-2 shadow-indigo-500 rounded-2xl text-center"
+          "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
         }
       >
-        <h1>星際大戰與機器人</h1>
-        <button
-          className={
-            "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
-          }
-        >
-          <Link to={"/robots"}>Robots</Link>
-        </button>
-      </div>
-      <div
+        星際大戰與機器人
+      </Link>
+      <Link
+        prefetch="intent"
+        to={"/robots"}
         className={
-          "m-5 shadow-2xl p-2 shadow-indigo-600 rounded-2xl text-center"
+          "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
         }
       >
-        <h1>星際大戰與機器人-預載入</h1>
-        <button
-          className={
-            "m-5 p-3 bg-blue-500 shadow-2xl shadow-indigo-400 rounded-2xl hover:bg-cyan-200"
-          }
-        >
-          <Link prefetch="intent" to={"/robots"}>
-            Robots intent
-          </Link>
-        </button>
-      </div>
+        星際大戰與機器人-預載入
+      </Link>
     </div>
   );
 }
