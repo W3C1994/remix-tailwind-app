708d80a874ef8cf5aa057836ab5656ff78f3986e
rick <edcrfvlily5@gmail.com>
2021-12-26 11:40:42 +0800

    [add]取得cookie中使用者資料如無法取得則導向login頁面 也可以在函數中加入特定導向路由在驗證失敗時作用
    可在此處加入後端
    驗證使用者服務

--- app/utils/session.server.js
+++ app/utils/session.server.js
@@ -49,6 +49,27 @@ const storage = createCookieSessionStorage({
   },
 });
 
+export function getUserSession(request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function requireUserId(request, redirectTo) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect(`/login?${searchParams}`);
+  }
+  return userId;
+}
+
 export async function createUserSession(userId, redirectTo) {
   const session = await storage.getSession();
   session.set("userId", userId);
